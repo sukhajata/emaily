26c4df7f1f8b93e458d74179530126fbf264170b
sukhajata <sukhajata@gmail.com>
2018-12-24 08:17:08 +0700

    Added purchase flow

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,17 +3,20 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', requireLogin, async (req, res) => {
+    app.post('/api/stripe', 
+        requireLogin, 
+        async (req, res) => {
 
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: 'Purchase credits',
-            source: req.body.id
-        });
-        
-        req.user.credits += 5;
-        const user = await req.user.save();
-        res.send(user);
-    });
+            const charge = await stripe.charges.create({
+                amount: 500,
+                currency: 'usd',
+                description: 'Purchase credits',
+                source: req.body.id
+            });
+            
+            req.user.credits += 5;
+            const user = await req.user.save();
+            res.send(user);
+        }
+    );
 };
---
+++ services/Mailer.js
@@ -0,0 +1,9 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+const Mailer = require( )
+class Mailer extends helper.Mail {
+
+}
+
+module.exports 
